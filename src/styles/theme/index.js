be7de9cecc474be5707af694bda0c38625f7f063
Matt <Richards>
2023-03-26 17:32:22 +0100

    Fixes broken import.

--- src/styles/theme/index.js
+++ src/styles/theme/index.js
@@ -1,6 +1,6 @@
 import colors from './colors'
 import typography from './typography'
-import breakPoints from './breakPoints'
+import breakPoints from './break-points'
 
 export default {
   colors,

99e32aeec6f0c714ff6e8846d32ad02dd6516890
Matt Richards <me@mttrchrds.com>
2024-03-11 23:18:12 +0000

    Removes holding page. Live!

--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 
 import ScreenTimeProvider from './providers/screen_time_provider'
-import ComingSoon from './components/pages/coming_soon'
 import Home from './components/pages/home'
 import Error from './components/pages/error'
 import ScreenTime from './components/pages/screen_time'
@@ -10,17 +9,16 @@ import ScreenTime from './components/pages/screen_time'
 export default createBrowserRouter([
   {
     path: '/',
-    element: <ComingSoon />,
-    errorElement: <Error />,
-  },
-  {
-    path: '/dev',
     element: <Home />,
     errorElement: <Error />,
   },
   {
     path: '/screentime',
-    element: <ScreenTimeProvider><ScreenTime /></ScreenTimeProvider>,
+    element: (
+      <ScreenTimeProvider>
+        <ScreenTime />
+      </ScreenTimeProvider>
+    ),
     errorElement: <Error />,
   },
 ])

3be2e20d67221293aba8fcd44e28449b748a0d4d
Matt Richards <me@mttrchrds.com>
2024-03-15 17:23:38 +0000

    Opens activity modal on click on smaller screens.

--- src/components/timeline/timeline_activity.js
+++ src/components/timeline/timeline_activity.js
@@ -9,6 +9,7 @@ import axios from 'axios'
 
 import { TimelineContext } from '../../providers/timeline_provider'
 import TimelineActivityTooltip from './timeline_activity_tooltip'
+import Activity from './activity'
 
 const StyledTimelineActivity = styled.div`
   position: relative;
@@ -18,6 +19,7 @@ const StyledTimelineActivity = styled.div`
   align-items: flex-end;
   justify-content: center;
   cursor: pointer;
+  user-select: none;
   .activity-head {
     position: absolute;
     z-index: 3;
@@ -72,11 +74,42 @@ const StyledTimelineActivity = styled.div`
       transition: all 0.1s ease-in-out;
     }
   }
+  .activity-modal {
+    position: fixed;
+    z-index: 10;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0.7);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &__header {
+      margin-bottom: 5px;
+      position: relative;
+      height: 24px;
+      &__icon {
+        position: absolute;
+        right: -10px;
+        top: 0;
+        svg {
+          fill: ${props => props.theme.colors.timeline.text1};
+        }
+      }
+    }
+    &__container {
+      width: 70%;
+      background-color: ${props => props.theme.colors.timeline.secondary};
+      padding: 10px 20px 20px 20px;
+      border-radius: 6px;
+    }
+  }
 `
 
 const tooltipWidth = 250
 
-const Activity = props => {
+const TimelineActivity = props => {
   const {
     setActiveActivity,
     activeActivity,
@@ -89,6 +122,7 @@ const Activity = props => {
   const [activityHover, setActivityHover] = useState(false)
   const [tooltipX, setTooltipX] = useState(0)
   const [tooltipY, setTooltipY] = useState(0)
+  const [displayActivityModal, setDisplayActivityModal] = useState(false)
 
   const timelineActivityRef = useRef(null)
 
@@ -119,6 +153,9 @@ const Activity = props => {
           if (payload) {
             setActiveActivity(payload)
             setActiveActivityLoading(false)
+            if (window.screen.width < 768) {
+              setDisplayActivityModal(true)
+            }
           }
         })
         .catch(error => {
@@ -144,6 +181,17 @@ const Activity = props => {
     const y = e.clientY - bounding.top
     setTooltipX(x)
     setTooltipY(y)
+  }
+
+  const handleClickActivityModal = e => {
+    if (e.target.className === 'activity-modal') {
+      setDisplayActivityModal(false)
+    }
+  }
+
+  const handleClickCloseIcon = e => {
+    e.preventDefault()
+    setDisplayActivityModal(false)
   }
 
   const calculateTooltipAlignment = () => {
@@ -178,6 +226,19 @@ const Activity = props => {
         width="24"
       >
         <path d="M189-160q-60 0-102.5-43T42-307q0-9 1-18t3-18l84-336q14-54 57-87.5t98-33.5h390q55 0 98 33.5t57 87.5l84 336q2 9 3.5 18.5T919-306q0 61-43.5 103.5T771-160q-42 0-78-22t-54-60l-28-58q-5-10-15-15t-21-5H385q-11 0-21 5t-15 15l-28 58q-18 38-54 60t-78 22Zm3-80q19 0 34.5-10t23.5-27l28-57q15-31 44-48.5t63-17.5h190q34 0 63 18t45 48l28 57q8 17 23.5 27t34.5 10q28 0 48-18.5t21-46.5q0 1-2-19l-84-335q-7-27-28-44t-49-17H285q-28 0-49.5 17T208-659l-84 335q-2 6-2 18 0 28 20.5 47t49.5 19Zm348-280q17 0 28.5-11.5T580-560q0-17-11.5-28.5T540-600q-17 0-28.5 11.5T500-560q0 17 11.5 28.5T540-520Zm80-80q17 0 28.5-11.5T660-640q0-17-11.5-28.5T620-680q-17 0-28.5 11.5T580-640q0 17 11.5 28.5T620-600Zm0 160q17 0 28.5-11.5T660-480q0-17-11.5-28.5T620-520q-17 0-28.5 11.5T580-480q0 17 11.5 28.5T620-440Zm80-80q17 0 28.5-11.5T740-560q0-17-11.5-28.5T700-600q-17 0-28.5 11.5T660-560q0 17 11.5 28.5T700-520Zm-360 60q13 0 21.5-8.5T370-490v-40h40q13 0 21.5-8.5T440-560q0-13-8.5-21.5T410-590h-40v-40q0-13-8.5-21.5T340-660q-13 0-21.5 8.5T310-630v40h-40q-13 0-21.5 8.5T240-560q0 13 8.5 21.5T270-530h40v40q0 13 8.5 21.5T340-460Zm140-20Z" />
+      </svg>
+    )
+  }
+
+  const renderCloseIcon = () => {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24"
+        viewBox="0 -960 960 960"
+        width="24"
+      >
+        <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
       </svg>
     )
   }
@@ -222,13 +283,30 @@ const Activity = props => {
           alignment={calculateTooltipAlignment()}
         />
       )}
+      {displayActivityModal && (
+        <div className="activity-modal" onClick={handleClickActivityModal}>
+          <div className="activity-modal__container">
+            <div className="activity-modal__header">
+              <div
+                className="activity-modal__header__icon"
+                onClick={handleClickCloseIcon}
+              >
+                {renderCloseIcon()}
+              </div>
+            </div>
+            <div className="activity-modal__body">
+              <Activity />
+            </div>
+          </div>
+        </div>
+      )}
     </StyledTimelineActivity>
   )
 }
 
-Activity.defaultProps = {}
+TimelineActivity.defaultProps = {}
 
-Activity.propTypes = {
+TimelineActivity.propTypes = {
   activityColour: PropTypes.string.isRequired,
   dayHeight: PropTypes.number.isRequired,
   channelWidth: PropTypes.number.isRequired,
@@ -243,4 +321,4 @@ Activity.propTypes = {
   channelIndex: PropTypes.number.isRequired,
 }
 
-export default Activity
+export default TimelineActivity

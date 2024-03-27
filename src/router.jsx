9d08555452fbcdc2e0e7ea12bb68ddfb878efec0
Matt Richards <me@mttrchrds.com>
2024-03-27 13:05:11 +0000

    Lazy loads timeline functionality.

--- src/router.jsx
+++ src/router.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
+import styled from 'styled-components'
 
 import Home from './components/pages/home'
 import Error from './components/pages/error'
-import Timeline from './components/pages/timeline'
+import HomeLoading from './components/home/home_loading'
+const Timeline = lazy(() => import('./components/pages/timeline'))
+
+const StyledTimelineLoading = styled.div`
+  height: calc(100vh - 80px);
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const renderTimelineLoading = () => (
+  <StyledTimelineLoading>
+    <HomeLoading />
+  </StyledTimelineLoading>
+)
 
 export default createBrowserRouter([
   {
@@ -13,7 +29,11 @@ export default createBrowserRouter([
   },
   {
     path: '/timeline',
-    element: <Timeline />,
+    element: (
+      <Suspense fallback={renderTimelineLoading()}>
+        <Timeline />
+      </Suspense>
+    ),
     errorElement: <Error />,
   },
 ])

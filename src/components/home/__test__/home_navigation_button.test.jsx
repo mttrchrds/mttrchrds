074afcf86c09958b0168f25a24a22a622676a6a0
Matt Richards <me@mttrchrds.com>
2024-04-19 15:57:51 +0100

    Fixes broken test.

--- src/components/home/__test__/home_navigation_button.test.jsx
+++ src/components/home/__test__/home_navigation_button.test.jsx
@@ -6,7 +6,9 @@ import render from '../../../testing/render'
 describe('HomeNavigationButton', () => {
   it('should render the label if passed to it', () => {
     const labelProp = 'Home'
-    render(<HomeNavigationButton label={labelProp} clickHandler={() => null} />)
+    render(
+      <HomeNavigationButton section={labelProp} clickHandler={() => null} />,
+    )
     const renderedLabel = screen.getByRole('link')
     expect(renderedLabel).toHaveTextContent(labelProp)
   })

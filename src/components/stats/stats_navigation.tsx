0c8ba69f7d114c61b05db27a238e861988de8ff4
Matt Richards <me@mttrchrds.com>
2024-05-06 18:21:47 +0100

    Refactors stats navigation to be more atomic.

--- src/components/stats/stats_navigation.tsx
+++ src/components/stats/stats_navigation.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState } from 'react'
+import { useState } from 'react'
 import styled from 'styled-components'
 
 import { StatsTab } from '../../helpers/enums'
@@ -7,12 +7,12 @@ import { useAppSelector, useAppDispatch } from '../../hooks/hooks'
 
 import { updateActiveTab } from '../../redux/stats/stats_slice'
 
-import theme from '../../styles/theme'
-
 import { mqMin } from '../../helpers/media_queries'
 
 import IconBurger from '../icons/icon_burger'
 import IconClose from '../icons/icon_close'
+import StatsNavigationItem from './navigation/stats_navigation_item'
+import StatsNavigationToggle from './navigation/stats_navigation_toggle'
 
 interface StyledStatsNavigationProps {
   $mobileNavOpen: boolean
@@ -21,22 +21,6 @@ interface StyledStatsNavigationProps {
 const StyledStatsNavigation = styled.nav<StyledStatsNavigationProps>`
   display: flex;
   flex-direction: column;
-  .mobile-header {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin: 0;
-    padding: 10px 15px;
-    border: 0;
-    background-color: ${props => props.theme.colors.stats.contentBackground};
-    border-bottom: 1px solid ${props => props.theme.colors.stats.border};
-    h3 {
-      margin: 0;
-    }
-    @media ${props => mqMin(props.theme.breakPoints.md)} {
-      display: none;
-    }
-  }
   .nav-items-container {
     display: ${props => (props.$mobileNavOpen ? 'block' : 'none')};
     @media ${props => mqMin(props.theme.breakPoints.md)} {
@@ -45,41 +29,6 @@ const StyledStatsNavigation = styled.nav<StyledStatsNavigationProps>`
   }
 `
 
-interface StyledStatsNavigationItemProps {
-  $active: boolean
-}
-
-const StyledStatsNavigationItem = styled.a<StyledStatsNavigationItemProps>`
-  &:link,
-  &:visited,
-  &:active {
-    color: ${props => props.theme.colors.stats.text};
-    text-decoration: none;
-  }
-  &:hover {
-    text-decoration: none;
-  }
-  display: block;
-  cursor: pointer;
-  padding: 10px 15px;
-  border-top: 1px solid ${props => props.theme.colors.stats.border};
-  font-weight: ${props => (props.$active ? 'bold' : 'normal')};
-  background-color: ${props => props.theme.colors.stats.background};
-  &:first-child {
-    border-top: 0;
-  }
-  &:last-child {
-    border-bottom: 1px solid ${props => props.theme.colors.stats.border};
-  }
-  @media ${props => mqMin(props.theme.breakPoints.md)} {
-    background-color: ${props => props.theme.colors.stats.contentBackground};
-  }
-`
-
-interface StatsNavigationProps {
-  activeTab: StatsTab
-}
-
 const StatsNavigation = () => {
   const dispatch = useAppDispatch()
 
@@ -87,37 +36,35 @@ const StatsNavigation = () => {
 
   const [mobileNavOpen, setMobileNavOpen] = useState(false)
 
-  const handleClickMobileHeader = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleClickNavigationToggle = (
+    e: React.MouseEvent<HTMLButtonElement>,
+  ) => {
     e.preventDefault()
     setMobileNavOpen(current => !current)
   }
 
   return (
     <StyledStatsNavigation $mobileNavOpen={mobileNavOpen}>
-      <button className="mobile-header" onClick={handleClickMobileHeader}>
-        <h3>View more stats</h3>
-        {mobileNavOpen ? (
-          <IconClose colour={theme.colors.stats.text} />
-        ) : (
-          <IconBurger colour={theme.colors.stats.text} />
-        )}
-      </button>
+      <StatsNavigationToggle
+        active={mobileNavOpen}
+        clickHandler={handleClickNavigationToggle}
+      />
       <div className="nav-items-container">
-        <StyledStatsNavigationItem
-          $active={activeTab === StatsTab.GAME_DAYS ? true : false}
-          onClick={e => {
+        <StatsNavigationItem
+          active={activeTab === StatsTab.GAME_DAYS ? true : false}
+          clickHandler={e => {
             e.preventDefault()
             dispatch(updateActiveTab(StatsTab.GAME_DAYS))
           }}
         >
           Most played games
-        </StyledStatsNavigationItem>
-        <StyledStatsNavigationItem
-          $active={false}
-          onClick={() => console.log('coming soon')}
+        </StatsNavigationItem>
+        <StatsNavigationItem
+          active={false}
+          clickHandler={e => console.log('coming soon', e)}
         >
           Games chart 2
-        </StyledStatsNavigationItem>
+        </StatsNavigationItem>
       </div>
     </StyledStatsNavigation>
   )

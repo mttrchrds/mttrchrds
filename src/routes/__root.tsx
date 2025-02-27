fd2f9ec66f1bed1dd9db0011516ad3b1ecb5b221
Matt Richards <me@mttrchrds.com>
2025-02-27 21:13:30 +0000

    Adds basic Page Not Found and Error pages to routes.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,8 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
+import Layout from '../components/layout/layout'
+
 export const Route = createRootRoute({
   component: () => (
     <>
@@ -8,4 +10,24 @@ export const Route = createRootRoute({
       <TanStackRouterDevtools />
     </>
   ),
+  notFoundComponent: () => {
+    return (
+      <Layout>
+        <div>
+          <p>Page not found!</p>
+          <Link to="/">Start Over</Link>
+        </div>
+      </Layout>
+    )
+  },
+  errorComponent: () => {
+    return (
+      <Layout>
+        <div>
+          <p>An error occurred! :(</p>
+          <Link to="/">Start Over</Link>
+        </div>
+      </Layout>
+    )
+  },
 })

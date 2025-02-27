59a7d4faaa5899a7bb1d25af0c1a16fdc319821a
Matt Richards <me@mttrchrds.com>
2025-02-27 20:28:15 +0000

    Installs react-query.

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import theme from './styles/theme'
-
 import GlobalStyles from './styles/global_styles'
 
 import { routeTree } from './routeTree.gen'
-
 const router = createRouter({ routeTree })
 
 const loadingPlaceholder = document.getElementById('loading') as HTMLElement
 loadingPlaceholder.style.display = 'none'
+
+const queryClient = new QueryClient()
 
 const rootElement = document.getElementById('app') as HTMLElement
 
@@ -20,10 +21,12 @@ if (!rootElement.innerHTML) {
   const root = createRoot(rootElement)
   root.render(
     <StrictMode>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <RouterProvider router={router} />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </QueryClientProvider>
     </StrictMode>,
   )
 }
